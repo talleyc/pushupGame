34e5342cfa902159d24cd4323a407d38347cfa30
Chris <ctalley@salesforce.com>
2015-01-02 16:00:42 -0800

    programmatic UI to fit all phone devices

--- Pushup Game/ViewController.swift
+++ Pushup Game/ViewController.swift
@@ -11,38 +11,30 @@ import Darwin
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var cardImage: UIImageView!
-    @IBOutlet weak var cardButton: UIButton!
-    @IBOutlet weak var reshuffleButton: UIButton!
-    @IBOutlet weak var currentPlayerNameLabel: UILabel!
+    var cardImage: UIImageView!
+    var currentPlayerNameLabel: UILabel!
     var instructionLabel :UILabel!
     var game :Game!
     var players :Array<Player>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         cardImage.layer.cornerRadius = 10
         cardImage.clipsToBounds = true
         cardImage.image = UIImage(named: "back_of_card.png")
-        instructionLabel = UILabel(frame:CGRectMake(-100,self.cardImage.center.y - 40,  self.cardImage.frame.width, 50))
-        instructionLabel.text = "SWIPE RIGHT TO START"
-        instructionLabel.textColor = UIColor(red: 0.03, green: 0, blue: 0.75, alpha: 1)
-        instructionLabel.textAlignment = NSTextAlignment.Center
-        instructionLabel.font = UIFont.boldSystemFontOfSize(17)
     }
     
     
     override func viewDidAppear(animated :Bool) {
-        instructionLabel.frame = CGRectMake(-100,self.cardImage.center.y - 40,  self.cardImage.frame.width, 50)
+        instructionLabel.frame = CGRectMake(-100,self.cardImage.frame.origin.y,  self.cardImage.frame.width, cardImage.frame.height)
         game = Game()
         instructionLabel.hidden = false
         UIView.animateWithDuration(0.7, delay:0.0, options: .CurveEaseOut, animations: {
             
             self.view.addSubview(self.instructionLabel)
             self.instructionLabel!.frame.origin.x = self.cardImage.frame.origin.x
-            }, completion: {finished in
-                println("showing instructions")
-            }
+            }, completion: {finished in}
         )
     }
 
@@ -62,16 +54,8 @@ class ViewController: UIViewController {
     }
     
     
-    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
-        if game != nil && game.players.count > 0 && game.players[0].turns.count > 0 {
-            //if game has been initialized, has players, and at least one turn has happened
-            return true
-        }
-        return false
-    }
     
-    
-    @IBAction func onTouchCard(recognizer: UIGestureRecognizer) {
+    func onTouchCard(sender: UIGestureRecognizer) {
         if game != nil && game.active {
             displayCard(game.startNextTurn())
         } else if game.stage == 0 {
@@ -79,6 +63,80 @@ class ViewController: UIViewController {
             game = Game()
             displayCard(game.startGame(players!))
         }
+    }
+    
+    func onClickEndGame(sender: UIButton) {
+        if game != nil && game.players.count > 0 && game.players[0].turns.count > 0 {
+            //if game has been initialized and at least one player has taken a turn
+            performSegueWithIdentifier("gameToResults", sender: sender)
+        }
+    }
+    
+    
+    /*
+        RENDERING LOGIC
+    */
+    
+    func setupView() {
+        placeNameLabel()
+        placeCardImage()
+        prepareInstructionLabel()
+        placeEndGameButton()
+    }
+    
+    func placeNameLabel() {
+        let labelWidth = self.view.frame.width
+        let labelHeight = CGFloat(30)
+        let labelX = CGFloat(0)
+        let labelY = self.view.frame.height * 0.06 + navigationController!.navigationBar.frame.height
+        currentPlayerNameLabel = UILabel(frame:CGRectMake(labelX,labelY,labelWidth, labelHeight))
+        currentPlayerNameLabel.textColor = UIColor(red: 1, green: 234/255, blue: 48/255, alpha: 1)
+        currentPlayerNameLabel.textAlignment = NSTextAlignment.Center
+        currentPlayerNameLabel.font = UIFont.boldSystemFontOfSize(20)
+        currentPlayerNameLabel.numberOfLines = 1
+        self.view.addSubview(currentPlayerNameLabel)
+    }
+    
+    func placeEndGameButton() {
+        let endGameButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        let buttonWidth = cardImage.frame.width / 1.618
+        let buttonHeight = 43/111 * buttonWidth
+        let buttonX = self.view.center.x - buttonWidth / 2
+        let buttonY = cardImage.frame.origin.y + cardImage.frame.height + buttonHeight / 2
+        
+        endGameButton.backgroundColor = UIColor(red: 230/255, green: 221/255, blue: 22/255, alpha: 0.5)
+        endGameButton.setTitle("End Game", forState: .Normal)
+        endGameButton.setTitleColor(UIColor(red: 1, green: 9/255, blue: 9/255, alpha: 1), forState: .Normal)
+        endGameButton.titleLabel!.font = UIFont.systemFontOfSize(18)
+        endGameButton.frame = CGRectMake(buttonX, buttonY, buttonWidth, buttonHeight)
+        endGameButton.layer.cornerRadius = 12
+        endGameButton.layer.masksToBounds = true
+        endGameButton.addTarget(self, action: "onClickEndGame:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(endGameButton)
+    }
+    
+
+    
+    func placeCardImage() {
+        let cardHeight = self.view.frame.height * 0.5
+        let cardWidth = 72 / 96 * cardHeight
+        let cardX = CGFloat(self.view.center.x - (cardWidth / 2))
+        let cardY = currentPlayerNameLabel.frame.origin.y + currentPlayerNameLabel.frame.height
+        cardImage = UIImageView(image: UIImage(named: "back_of_card.png"))
+        cardImage!.frame = CGRectMake(cardX, cardY, cardWidth, cardHeight)
+        let swipe = UISwipeGestureRecognizer(target: self, action: Selector("onTouchCard:"))
+        cardImage!.addGestureRecognizer(swipe)
+        cardImage!.userInteractionEnabled = true
+        self.view.addSubview(cardImage)
+    }
+    
+    func prepareInstructionLabel() {
+        instructionLabel = UILabel(frame:CGRectMake(-100,self.cardImage.center.y - 40,  self.cardImage.frame.width, cardImage.frame.height))
+        instructionLabel.text = "SWIPE RIGHT TO START"
+        instructionLabel.textColor = UIColor(red: 0.03, green: 0, blue: 0.75, alpha: 1)
+        instructionLabel.textAlignment = NSTextAlignment.Center
+        instructionLabel.font = UIFont.boldSystemFontOfSize(22)
+        instructionLabel.numberOfLines = 2
     }
     
     func displayCard(card :Card) {
--- SetupController.swift
+++ SetupController.swift
@@ -10,13 +10,16 @@ import Foundation
 import UIKit
 
 class SetupController: UIViewController {
-    @IBOutlet weak var player1NameField: UITextField!
-    @IBOutlet weak var player2NameField: UITextField!
-    @IBOutlet weak var player3NameField: UITextField!
-    @IBOutlet weak var player4NameField: UITextField!
-    var actualPlayers :Array<Player>!
+    var player1NameField: UITextField!
+    var player2NameField: UITextField!
+    var player3NameField: UITextField!
+    var player4NameField: UITextField!
+    var actualPlayers: Array<Player>!
+    var separator: UIView!
+    var goButton: UIButton!
     
     override func viewDidLoad() {
+        setupPage()
         var nameFields = getNameFields()
         let userDefaults = NSUserDefaults.standardUserDefaults()
         for i in 1...4 {
@@ -74,4 +77,109 @@ class SetupController: UIViewController {
         return playerFields
     }
     
+    func onClickGo(sender: UIButton) {
+        
+        actualPlayers = getInsertedPlayers()
+        if actualPlayers.count < 2 {
+            var alert = UIAlertController(title: "Need more players", message: "Must have at least 2 players to participate", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            performSegueWithIdentifier("setupToGame", sender: goButton)
+        }
+    }
+    
+    /*
+        RENDERING LOGIC
+    */
+    func setupPage() {
+        addInstructionLabel()
+        addNameFields()
+        addGoButton()
+    }
+    
+    func addInstructionLabel() {
+        let labelWidth = self.view.frame.width
+        let labelHeight = CGFloat(35)
+        let labelX = CGFloat(0)
+        let labelY = navigationController!.navigationBar.frame.height + self.view.frame.height * 0.06
+        let instructionLabel = UILabel(frame: CGRectMake(labelX, labelY, labelWidth, labelHeight))
+        instructionLabel.textAlignment = NSTextAlignment.Center
+        instructionLabel.textColor = UIColor(red: 1, green: 234/255, blue: 48/255, alpha: 1)
+        instructionLabel.font = UIFont.boldSystemFontOfSize(20)
+        instructionLabel.text = "Enter Up To 4 Names"
+        self.view.addSubview(instructionLabel)
+        //add separator view
+        let separatorWidth = self.view.frame.width * 0.9
+        let separatorHeight = CGFloat(5)
+        let separatorX = self.view.frame.width * 0.05
+        let separatorY = labelY + labelHeight + self.view.frame.height * 0.02
+        
+        separator = UIView(frame: CGRectMake(separatorX, separatorY, separatorWidth, separatorHeight))
+        separator.backgroundColor = UIColor(red: 159/255, green: 230/255, blue: 105/255, alpha: 1)
+        self.view.addSubview(separator)
+    }
+    
+    func addNameFields() {
+        
+        let fieldWidth = self.view.frame.width / 3
+        let fieldHeight = fieldWidth * 3 / 10
+        let fieldX = fieldWidth
+        let startFieldY = separator.frame.origin.y + separator.frame.height + self.view.frame.height * 0.05
+        let fieldGap = self.view.frame.height * 0.02
+        player1NameField = UITextField(frame: CGRectMake(fieldX, startFieldY, fieldWidth, fieldHeight))
+        player2NameField = UITextField(frame: CGRectMake(fieldX, startFieldY + fieldHeight + fieldGap, fieldWidth, fieldHeight))
+        player3NameField = UITextField(frame: CGRectMake(fieldX, startFieldY + fieldHeight * 2 + fieldGap * 2, fieldWidth, fieldHeight))
+        player4NameField = UITextField(frame: CGRectMake(fieldX, startFieldY + fieldHeight * 3 + fieldGap * 3, fieldWidth, fieldHeight))
+        setupNameField(player1NameField, index: 1)
+        setupNameField(player2NameField, index: 2)
+        setupNameField(player3NameField, index: 3)
+        setupNameField(player4NameField, index: 4)
+        
+    }
+    
+    func addGoButton() {
+        goButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        let buttonWidth = player1NameField.frame.width / 1.618
+        let buttonHeight = buttonWidth / 1.618
+        let buttonX = self.view.center.x - buttonWidth / 2
+        let buttonY = player4NameField.frame.origin.y + player4NameField.frame.height + buttonHeight / 2
+        goButton.backgroundColor = UIColor(red: 230/255, green: 221/255, blue: 22/255, alpha: 0.5)
+        
+        goButton.setTitle("GO!", forState: .Normal)
+        goButton.setTitleColor(UIColor(red: 1, green: 9/255, blue: 9/255, alpha: 1), forState: .Normal)
+        goButton.titleLabel!.font = UIFont.systemFontOfSize(18)
+        goButton.frame = CGRectMake(buttonX, buttonY, buttonWidth, buttonHeight)
+        goButton.layer.cornerRadius = 12
+        goButton.layer.masksToBounds = true
+        goButton.addTarget(self, action: "onClickGo:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(goButton)
+    }
+    
+    func setupNameField(field :UITextField, index :Int) {
+        field.borderStyle = UITextBorderStyle.RoundedRect
+        field.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.6)
+        field.placeholder = "Contender \(index)"
+        self.view.addSubview(field)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
